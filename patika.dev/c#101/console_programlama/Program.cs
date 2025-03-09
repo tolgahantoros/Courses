361693b5fdb3636ad819f5de77926da42473a174
Tolgahan Toros <tolgahantoros@gmail.com>
2025-03-09 03:17:48 +0300

    c#101start

---
+++ patika.dev/c#101/console_programlama/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello World! I'm TT");
+Console.WriteLine("İsminizi Girin:");
+string name = Console.ReadLine();
+Console.WriteLine("Soyisminizi Girin:");
+string surName = Console.ReadLine();
+Console.WriteLine("Merhaba " + name + " " + surName);
